6394aba66afea1656e21c60e837f78fc6c22bf2d
MaisClement <clementf78@gmail.com>
2022-12-05 15:01:22 +0100

    add journeys

--- src/v0.1/journeys.php
+++ src/v0.1/journeys.php
@@ -32,12 +32,90 @@ $results = json_decode($results);
 
 $journeys = [];
 foreach($results->journeys as $result){
-    echo json_encode($result);
-    exit;
     
+    $sections = [];
+    foreach($result->sections as $section) {
+        if (isset($section->display_informations)) {
+            $informations = array(
+                "direction" => array(
+                    "id"        =>  (String)    $section->display_informations->direction,
+                    "name"      =>  (String)    "",
+                ),
+                "id"            =>  (String)    $result->ItemIdentifier,
+                "name"          =>  (String)    $section->display_informations->name,
+                "mode"          =>  (String)    $section->display_informations->physical_mode,
+                "trip_name"     =>  (String)    $section->display_informations->trip_short_name,
+                "headsign"      =>  (String)    $section->display_informations->headsign,
+                "description"   =>  (String)    $section->display_informations->description,
+                "message"       =>  (String)    "",
+                "line"     => array(
+                    "id"         =>  (String)   $line_id,
+                    "code"       =>  (String)   $section->display_informations->code,
+                    "name"       =>  (String)   $section->display_informations->network . $section->display_informations->name,
+                    "mode"       =>  (String)   $section->display_informations->physical_mode,
+                    "color"      =>  (String)   $section->display_informations->color,
+                    "text_color" =>  (String)   $section->display_informations->text_color,
+                ),
+            );
+        }
+        
+        $sections[] = array(
+            "id"            =>  (String) $section->id,
+            "type"          =>  (String) $section->type,
+            "mode"          =>  (String) isset($section->mode) ? $section->mode : $section->type,
+            "arrival_date_time"     =>  (String) $section->arrival_date_time,
+            "departure_date_time"   =>  (String) $section->departure_date_time,
+            "duration"      =>  (String) $section->duration,
+            "informations"  => isset($informations) ? $informations : [],
+            "from" => array(
+                "id"        =>  (String)    $section->from->id,
+                "name"      =>  (String)    $section->from->{$section->from->embedded_type}->name,
+                "type"      =>  (String)    $section->from->embedded_type,
+                // "quality"   =>  (int)       0,
+                // "distance"  =>  (int)       isset($section->from->distance) ? $section->from->distance : 0,
+                "zone"      =>  (int)       0,
+                "town"      =>  (String)    getTownByAdministrativeRegions( $section->from->{$section->from->embedded_type}->administrative_regions ),
+                "zip_code"  =>  (String)    getZipCodeByInsee( getZipByAdministrativeRegions( $section->from->{$section->from->embedded_type}->administrative_regions ) )->fetch()['zip_code'],
+                "coord"     => array(
+                    "lat"           =>  floatval( $section->from->{$section->from->embedded_type}->coord->lat ),
+                    "lon"           =>  floatval( $section->from->{$section->from->embedded_type}->coord->lon ),
+                ),
+            ),
+            "to" => array(
+                "id"        =>  (String)    $section->to->id,
+                "name"      =>  (String)    $section->to->{$section->to->embedded_type}->name,
+                "type"      =>  (String)    $section->to->embedded_type,
+                // "quality"   =>  (int)       0,
+                // "distance"  =>  (int)       isset($section->to->distance) ? $section->to->distance : 0,
+                "zone"      =>  (int)       0,
+                "town"      =>  (String)    getTownByAdministrativeRegions( $section->to->{$section->to->embedded_type}->administrative_regions ),
+                "zip_code"  =>  (String)    getZipCodeByInsee( getZipByAdministrativeRegions( $section->to->{$section->to->embedded_type}->administrative_regions ) )->fetch()['zip_code'],
+                "coord"     => array(
+                    "lat"       =>  floatval( $section->to->{$section->to->embedded_type}->coord->lat ),
+                    "lon"       =>  floatval( $section->to->{$section->to->embedded_type}->coord->lon ),
+                ),
+            ),
+
+        );
+    }
+
+    $journeys[] = array(
+        "type"                  =>  (String) $result->type,
+        "duration"              =>  (String) $result->duration,
+
+        "requested_date_time"   => $result->requested_date_time,
+        "departure_date_time"   => $result->departure_date_time,
+        "arrival_date_time"     => $result->arrival_date_time,
+        
+        "nb_transfers"          =>  (int)    floatval( $result->type ),
+        "co2_emission"          => $result->co2_emission,
+        "distances"             => $result->distances,
+        "sections"              => $sections
+    );
 }
 
 $json = [];
+$json['journeys'] = $journeys;
 $echo = json_encode($json);
 file_put_contents($fichier, $echo);
 echo $echo;
